89b880fba0adcd557bccd1604873568e1e88019d
Jake-Ravett <127255169+Jake-Ravett@users.noreply.github.com>
2023-05-02 19:15:40 -0400

    Update hello.c

--- ExerciseSet1/hello.c
+++ ExerciseSet1/hello.c
@@ -3,10 +3,9 @@ A first program in C */
 
 #include <stdio.h>
 
-int main()
-{
-printf("Hello Your name\n");
-printf("Welcome to EEE1008\n");
-return 0;
+int main(){
+  printf("Hello Your name\n");
+  printf("Welcome to EEE1008\n");
+  return 0;
 }
 
