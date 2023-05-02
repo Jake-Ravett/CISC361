d90cb24e6ea41940897edfb93b61f344caa2124b
Jake-Ravett <127255169+Jake-Ravett@users.noreply.github.com>
2023-05-02 19:05:19 -0400

    Add files via upload

---
+++ ExerciseSet1/fork.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main()
+{
+    int status;
+    int pid = fork();
+
+    // Write C code here
+    if (pid == 0)
+    { // child
+        int pid1 = fork();
+        if (pid1 == 0)
+        {
+            int pid2 = getpid();
+            printf("I am the grandchild %d\n", pid2);
+        }
+        else
+        {
+            int pid2 = getpid();
+            printf("I am the child %d\n", pid2);
+        }
+    }
+    else
+    {
+        int pid1 = getpid();
+        printf("I am the parent %d\n", pid1);
+    }
+    return 0;
+}
