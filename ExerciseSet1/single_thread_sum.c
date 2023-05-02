c32cc642a8ce4fa1faf2dbbd2df25540fe6ff700
Jake-Ravett <127255169+Jake-Ravett@users.noreply.github.com>
2023-05-02 19:16:57 -0400

    Update single_thread_sum.c

--- ExerciseSet1/single_thread_sum.c
+++ ExerciseSet1/single_thread_sum.c
@@ -7,16 +7,15 @@ static int arr[MAX_NO_OF_ELEMENTS];
 int main(){
 //let the array consists of first MAX_NO_OF_ELEMENTS integers,
 //1 to MAX_NO_OF_ELEMENTS
-	for (int i = 0; i < MAX_NO_OF_ELEMENTS; i++){
-		arr[i] = i + 1;
-	}
-//Setup timing
-clock_t start, end;
-double cpu_time_taken;
-start = clock();
+    for (int i = 0; i < MAX_NO_OF_ELEMENTS; i++){
+       arr[i] = i + 1;
+    }
+   //Setup timing
+    clock_t start, end;
+    double cpu_time_taken;
+    start = clock();
 
-
-printf("Total sum: %lld\n", sum);
-printf("Time taken to sum all the numbers are %lf\n", cpu_time_taken);
-return 0;
+    printf("Total sum: %lld\n", sum);
+    printf("Time taken to sum all the numbers are %lf\n", cpu_time_taken);
+    return 0;
 }
