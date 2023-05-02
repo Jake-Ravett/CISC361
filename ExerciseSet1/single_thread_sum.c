1a8eca1ef4ffa6beb32019ab3171be547f025a15
Jake-Ravett <127255169+Jake-Ravett@users.noreply.github.com>
2023-05-02 19:14:55 -0400

    Update single_thread_sum.c

--- ExerciseSet1/single_thread_sum.c
+++ ExerciseSet1/single_thread_sum.c
@@ -4,13 +4,12 @@
 #define MAX_NO_OF_ELEMENTS 100000000
 static long long int sum;
 static int arr[MAX_NO_OF_ELEMENTS];
-int main()
-{
+int main(){
 //let the array consists of first MAX_NO_OF_ELEMENTS integers,
 //1 to MAX_NO_OF_ELEMENTS
-for (int i = 0; i < MAX_NO_OF_ELEMENTS; i++)
-	arr[i] = i + 1;
-
+	for (int i = 0; i < MAX_NO_OF_ELEMENTS; i++){
+		arr[i] = i + 1;
+	}
 //Setup timing
 clock_t start, end;
 double cpu_time_taken;
