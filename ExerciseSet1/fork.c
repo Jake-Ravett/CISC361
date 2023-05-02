40d29e1c3cbdb070879924e91a7c7897ab2d67f1
Jake-Ravett <127255169+Jake-Ravett@users.noreply.github.com>
2023-05-02 19:13:59 -0400

    Update fork.c

--- ExerciseSet1/fork.c
+++ ExerciseSet1/fork.c
@@ -4,28 +4,23 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-int main()
-{
+int main(){
     int status;
     int pid = fork();
 
     // Write C code here
-    if (pid == 0)
-    { // child
+    if (pid == 0){ // child
         int pid1 = fork();
-        if (pid1 == 0)
-        {
+        if (pid1 == 0){
             int pid2 = getpid();
             printf("I am the grandchild %d\n", pid2);
         }
-        else
-        {
+        else{
             int pid2 = getpid();
             printf("I am the child %d\n", pid2);
         }
     }
-    else
-    {
+    else{
         int pid1 = getpid();
         printf("I am the parent %d\n", pid1);
     }
